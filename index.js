40c0b7675182470eaca5ee501f6dea65495a2371
Dav <david.sanchez.martinez@outlook.com>
2025-08-28 16:15:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import { tweetsData } from './data.js'
 const tweetInput = document.getElementById('tweet-input')
-const tweetBtn = document.getElementById('tweet-btn')
-
-tweetBtn.addEventListener('click', function () {
-    console.log(tweetInput.value)
-})
 
 document.addEventListener('click', function (e) {
     if (e.target.dataset.like) {
@@ -16,6 +11,8 @@ document.addEventListener('click', function (e) {
     else if (e.target.dataset.reply) {
         handleReplyClick(e.target.dataset.reply)
     }
+    else if (e.target.id === 'tweet-btn')
+        handleTweetBtnClick()
 })
 
 function handleLikeClick(tweetId) {
@@ -40,6 +37,10 @@ function handleRetweetClick(tweetId) {
 
 function handleReplyClick(replyId){
     document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick() {
+    console.log(tweetInput.value)
 }
 
 function getFeedHtml() {
